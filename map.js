9941c4d05a09a95d5dc15e5e0d7d1c8d8cd6ebb2
1suo <isuo1991@outlook.com>
2017-11-14 22:49:54 +0300

    Delete map.js

--- map.js
+++
@@ -1,9 +0,0 @@
-        ymaps.ready(init);
- 
-        function init () {
-            var myMap = new ymaps.Map('map', {
-                    center: [44.96, 34.11], 
-                    zoom: 11
-                });
-				 myMap.options.set('scrollZoomSpeed', 1);
-        }
