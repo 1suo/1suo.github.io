494e6c83aa844c7feef34d5bcf2e5e2b0d8c5a63
1suo <isuo1991@outlook.com>
2019-03-19 19:51:34 +0300

    add tf&tfvis

---
+++ script.js
@@ -0,0 +1,30 @@
+async function getData(){
+    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+    const carsData = await carsDataReq.json();
+    const cleaned = carsData.map(car => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
+    })) 
+    .filter(car => (car.mpg != null && car.horsepower != null));
+    return cleaned;
+}
+
+async function run(){
+    const data = await getData();
+    const values = data.map(d=>({
+        x:d.horsepower,
+        y:d.mpg,
+    }));
+    tfvis.render.scatterplot(
+        {name:'Horsepower v MPG'},
+        {values},
+        {
+            xLabel:'Horsepower',
+            yLabel:'MPG',
+            height:300
+        }
+    );
+
+}
+
+document.addEventListener('DOMContentLoaded',run);
