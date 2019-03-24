f33eb9825dae600809d5f82e8b989b1afb3e67bb
1suo <isuo1991@outlook.com>
2019-03-24 13:39:47 +0300

    add predict

--- script.js
+++ script.js
@@ -61,6 +61,28 @@ async function trainModel(model, inputs, labels) {
     });
 }
 
+function testModel(model, inputData, normalizationData) {
+    const { inputMax, inputMin, labelMin, labelMax } = normalizationData;
+    const [xs, preds] = tf.tidy(() => {
+        const xs = tf.linspace(0, 1, 100);
+        const preds = model.predict(xs.reshape([100, 1]));
+        const unNormXs = xs.mul(inputMax.sub(inputMin)).add(inputMin);
+        const unNormPreds = preds.mul(labelMax.sub(labelMin).add(labelMin));
+        return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
+    const predictedPoints = Array.from(xs).map((val, i) => {
+        return { x: val, y: preds[i] };
+    });
+    const originalPoints = inputData.map(d => ({
+        x: d.horsepower, y: d.acc,
+    }));
+    tfvis.render.scatterplot(
+        { name: 'Model Predictions vs Original Data' },
+        { values: [originalPoints, predictedPoints], series: ['original', 'predicted'] },
+        { xLabel: 'Horsepower', yLabel: 'Acceleration', height: 300 }
+    );
+}
+
 async function run() {
     const data = await getData();
     const values = data.map(d => ({
@@ -82,6 +104,7 @@ async function run() {
     const { inputs, labels } = tensorData;
     await trainModel(model, inputs, labels);
     console.log('Done Training');
+    testModel(model, data, tensorData);
 }
 
 
