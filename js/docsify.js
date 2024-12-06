7cff8ec4cb0c5c72cb544811d258beb0f74373f2
1suo <fnoeordie@mail.ru>
2024-12-06 14:10:13 +0400

    luma worklog update

--- js/docsify.js
+++ js/docsify.js
@@ -11,4 +11,28 @@ window.$docsify = {
     ],
     displayMath: [["$$", "$$"]],
   },
+  markdown: {
+    renderer: {
+      //render checkbox as emoji
+      checkbox: function (checked) {
+        return checked ? "☑️" : "🔲";
+      },
+      listitem: function (text) {
+        //render list item as div element
+        return `<div class="list-item">${text}</div>`;
+      },
+      paragraph: function (text) {
+        //if has "kanban:settings" or "kanban-plugin" in the text, don't render
+        if (text.includes("kanban") || text.includes("%%")) {
+          return "";
+        }
+      },
+      code: function (code, lang) {
+        //if has "kanban:settings" or "kanban-plugin" in the text, don't render
+        if (code.includes("kanban")) {
+	  return "";
+        }
+      },
+    },
+  },
 };
