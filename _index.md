0d339a030de03ea293bf83eb3da603eff610728f
1suo <isuo1991@outlook.com>
2025-07-12 18:49:23 +0400

    upd bio, slight theme tweaks

--- _index.md
+++ _index.md
@@ -1,13 +1,23 @@
-##### 🧔 Roman Kalashnikov
-
-- Frontend (Vue, Three.js, Rust/Wasm)
-- Computational design (Rhino/Grasshopper)
+### ME
+*name:*  Roman Kalashnikov
+*origin:* 1991, Simferopol, Crimea🏴 
+*occupation:*
+- frontend engineering, mostly 3d and efficient data streaming (Vue, Three.js, WebGL, xstate, webRTC, ffmpeg, SQLite, indexedDB, wasm)
+- 3D/2D/CAD/BIM, mostly automating CAD and BIM workflows (Rhino, Grasshopper, FreeCAD, Blender, Geometry Nodes)
 - Ergonomic keyboards and layouts
----
-
-<!-- <button href="https://google.com" onclick="location.href='https://t.me/flowercraft_bot'" style="background-color: springgreen; color: #000; border: 0; font-weight: 600; border-radius: 1.5rem; padding: 0 0.8rem 0 0.3rem; box-shadow: 0 0 160px 80px #0F63, 0 0 8px 2px #6FC4, 0 0 80px 20px #6FC4;"><i class="fa-brands fa-telegram"></i>Play</button> -->
 
 ---
 
+### EDENICA
+Edenica is web3 game I'm working on right now. 
+Create beautiful procedural plants by selecting visually appealing mutations, and mint them on TON network.
+<!-- Visit [website](https://edenica.com) for more info or press button below. -->
 
-- [Как сделан этот сайт](md/site)
+ <button href="https://google.com" onclick="location.href='https://t.me/flowercraft_bot/Flowercraft'" style="background-color: springgreen; color: #000; border: 0; font-weight: 600; border-radius: 1.5rem; padding: 0 0.8rem 0 0.3rem; box-shadow: 0 0 160px 80px #0F63, 0 0 8px 2px #6FC4, 0 0 80px 20px #6FC4;"><i class="fa-brands fa-telegram"></i>Play beta</button>
+
+Feel free to provide feedback via [ telegram ](https://t.me/i1991).
+
+---
+
+
+<!-- Check this [website template](https://github.com/1suo/homepage_template) -->
--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,24 +1,32 @@
 window.onload = function () {
+  // let dark = "🌚";
+  // let light = "🌞";
+  let dark = "dark";
+  let light = "light";
   let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "🌚"
-    : "🌞";
+    ? dark
+    : "light";
   let body = document.documentElement;
+  let nav = document.getElementsByTagName("nav")[0];
 
   let button = document.getElementById("theme-switcher");
   button.style.cursor = "pointer";
-  button.innerText = theme == "🌚" ? "🌞" : "🌚";
+  button.innerText = theme == dark ? light : dark;
   button.style.float = "right";
   setTheme(theme);
 
   button.addEventListener("click", () => {
-    setTheme(theme == "🌚" ? "🌞" : "🌚");
+    setTheme(theme == dark ? light : dark);
   });
 
   function setTheme(target) {
-    if (target === "🌚") {
-      theme = "🌚";
+    if (target === dark) {
+      theme = dark;
       body.style.backgroundColor = "black";
-      body.style.color = "white";
+      body.style.color = "beige";
+      // nav.style.backgroundColor = "black";
+      // nav.style.color = "white";
+      // nav.style.borderColor = "#202020";
 
       const links = document.querySelectorAll("a");
       links.forEach((link) => {
@@ -26,11 +34,14 @@ window.onload = function () {
         link.style.setProperty("--hover", "yellow");
         link.style.setProperty("--visited", "magenta");
       });
-      button.innerText = "🌞";
+      button.innerText = light;
     } else {
-      theme = "🌞";
+      theme = light;
       body.style.backgroundColor = "whitesmoke";
       body.style.color = "black";
+      // nav.style.backgroundColor = "whitesmoke";
+      // nav.style.color = "black";
+      // nav.style.borderColor = "gainsboro";
 
       const links = document.querySelectorAll("a");
       links.forEach((link) => {
@@ -38,7 +49,7 @@ window.onload = function () {
         link.style.setProperty("--hover", "cyan");
         link.style.setProperty("--visited", "magenta");
       });
-      button.innerText = "🌚";
+      button.innerText = dark;
     }
   }
 };
